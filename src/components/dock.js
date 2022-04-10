e958e961658326c216b15dcab226e617cfe67246
M. Wierzbicki <michalwierzbicki.miw@gmail.com>
2022-04-10 14:49:34 +0200

    stylized dock

--- src/components/dock.js
+++ src/components/dock.js
@@ -52,27 +52,40 @@ const Dock = (props) => {
 
     const DockElement = (el, index) => {
         const [details, setDetails] = useState(false)
+        const [quickGuns, setQuickGuns] = useState(false)
         return (
             <div className='equip-main'>
-                <div className={details ? 'equip-true' : 'equip-false'} onClick={() => setDetails(!details)}>
+                <div className={details ? 'equip-true' : 'equip-false'}>
                     <img src={imgFleet[index]} alt={el.name} />
                     <div className={details ? 'equip_description-true' : 'equip_description-false'}>
                         {details ?
                             <div className='equip_title-true'>
-                                <h1>{el.name} (Ilość: {el.quantity})</h1>
+                                <h1 onClick={() => setDetails(!details)}>{el.name} (Ilość: {el.quantity})</h1>
                                 <p>{el.description}</p>
                             </div> :
                             <div className='equip_title-false'>
-                                <h1>{el.name} (Ilość: {el.quantity})</h1>
+                                <h1 onClick={() => {setDetails(!details); setQuickGuns(false)}}>{el.name} (Ilość: {el.quantity})</h1>
                             </div>
                         }
                         {details ?
                             <div className='equip_details-true'>
                                 <div className='equip_gun-true'>
-                                    {el.quickGunPlus.map((gun, index) => (
-                                        <div className="quick-gun"><p key={index}>{gun.description}</p><p>{gun.value}</p></div>
-                                        
-                                    ))}
+                                    <p onClick={() => setQuickGuns(!quickGuns)} className='quick-gun_relase' style={quickGuns ? {fontWeight: 500} : {}}>Szybkie działa ➾</p>
+                                    {quickGuns ?
+                                        <div className="quick-gun_main">
+                                            {el.quickGunPlus.length === 0 ? 
+                                            <p style={{color: 'red', fontWeight: 400}}>Brak szybkich dział przeciwko innym statkom!</p> : 
+                                            el.quickGunPlus.map((gun, index) => (
+                                                <div className="quick-gun"><p key={index}>{gun.description}</p><p style={{color: 'green', fontWeight: 700}}>{gun.value}</p></div>
+                                            ))}
+                                            <hr/>
+                                            {el.quickGunMinus.length === 0 ? 
+                                            <p style={{color: 'green', fontWeight: 400}}>Brak szybkich dział przeciwko {el.name}!</p>:
+                                            el.quickGunMinus.map((gun, index) => (
+                                                <div className="quick-gun"><p key={index}>{gun.description}</p><p style={{color: 'red', fontWeight: 500}}>{gun.value}</p></div>
+                                            ))}
+                                        </div> :
+                                        <></>}
                                 </div>
                                 <div className={details ? 'equip_cost-true' : 'equip_cost-false'}>
                                     <p>Metal: {el.cost.metal}</p>
